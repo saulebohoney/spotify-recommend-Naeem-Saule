4e6708db397c0a380a45235f92ae364710f09896
Saule BN <beyonce@thinkful.com>
2017-05-25 12:42:22 -0400

    work in progress

--- spotify.js
+++ spotify.js
@@ -9,22 +9,36 @@ var getFromApi = function(endpoint, query={}) {
     });
 };
 
-
 var artist;
 var getArtist = function(name) {
     const query = {
         q: name,
         limit: 1,
         type: 'artist'
-        }
+    };
+    const url = 'https://api.spotify.com/v1';
+    fetch(url).then(item => {
+  
+        var artist = item.artists.items[0];
+        return artist;
+    })
+    
+};
+     getFromApi(search,query);
 
-    const url = 'https://api.spotify.com/v1/${endpoint}';
-        // Edit me!
-//    ('https://api.spotify.com/v1/search' query, )
-        $.getJSON(url, query).then(stream => {
-            console.log('this',stream);
-            stream.json()
-        })
-}
 
-getArtist('DREAM');
+
+
+
+//     const url = 'https://api.spotify.com/v1/${endpoint}';
+//         // Edit me!
+// //    ('https://api.spotify.com/v1/search' query, )
+//     $.getJSON(url, query).then(stream => {
+//             console.log('this',stream);
+//             stream.json();
+//         });
+// };
+
+// getArtist('DREAM');
+
+//$.getJSON(url, query).then
