3f0bae94803c3805840a15da0c68a99904077242
PROB8 <blackb8r@gmail.com>
2017-05-25 15:43:44 -0400

    artistId retrieved

--- spotify.js
+++ spotify.js
@@ -19,19 +19,32 @@ var getArtist = function(name) {
         type: 'artist'
     };
 
-    const url = 'https://api.spotify.com/v1/search';
- 
-          
-        return getFromApi('search', query).then( item => {
+    return getFromApi('search', query).then( item => {
                 artist = item.artists.items[0];
-                console.log(artist);
-                return artist;
-        }).catch(function(err) {
-            console.log(err);
-        });
+               let artistId = item.artists.items[0].id;
+                console.log(artistId);
+                return  artistId;
+         }) .then(return fetch('https://api.spotify.com/v1/${artists}/${artistId}/related-artists').then(response =>{
+            console.log(response)
+         }))
+     // return .then( response =>{
+    
+     //        if (!response.ok) {
+     //            return Promise.reject(response.statusText);
+     //        }
+    
+     //        return response.json()
 
-        //console.log(artist);
+     //        })
+
+        //     .then(getFromApi('search', query).then( item => {
+        //         artist = item.artists.items[0].id;
+        //         console.log(artist);
+        //         return artist;
+        // }).catch( err => {
+        //     console.log(err);
+        // }))
+
     
 }
-//getArtist('stevie wonder');
 
