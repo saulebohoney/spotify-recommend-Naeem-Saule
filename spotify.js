207cf760051c4b33e2bb038898ed7adbeca318d8
Saule BN <beyonce@thinkful.com>
2017-05-25 16:20:59 -0400

    relatedArtists

--- spotify.js
+++ spotify.js
@@ -10,6 +10,7 @@ var getFromApi = function(endpoint, query={}) {
 };
 
 var artist;
+
 var getArtist = function(name) {
 
 
@@ -20,31 +21,45 @@ var getArtist = function(name) {
     };
 
     return getFromApi('search', query).then( item => {
+        artist = item.artists.items[0];
+        let artistId = item.artists.items[0].id;
+        console.log(artistId);
+        return  fetch('https://api.spotify.com/v1/artist/${artistId}/related-artists');
+    }).then((item)=> {
+             return item.json();
+             console.log(item);
+         });
+};
+        
+
+var getArtist = function(name) {
+
+
+    const query = {
+        q: name,
+        limit: 1,
+        type: 'artist'
+    };
+
+    //const url = 'https://api.spotify.com/v1/search';
+ 
+          
+        return getFromApi('search', query).then( item => {
                 artist = item.artists.items[0];
-               let artistId = item.artists.items[0].id;
-                console.log(artistId);
-                return  artistId;
-         }) .then(return fetch('https://api.spotify.com/v1/${artists}/${artistId}/related-artists').then(response =>{
-            console.log(response)
-         }))
-     // return .then( response =>{
-    
-     //        if (!response.ok) {
-     //            return Promise.reject(response.statusText);
-     //        }
-    
-     //        return response.json()
+                const artistId = item.artists.items[0].id;
+                console.log(artist);
+                return getFromApi(`artists/${artistId}/related-artists`);
+        }).then( item => {
+            artist.related =item.artists;
+                console.log(item);
+                return artist;
+                
+        }).catch(function(err) {
+            console.log(err);
+        });
 
-     //        })
-
-        //     .then(getFromApi('search', query).then( item => {
-        //         artist = item.artists.items[0].id;
-        //         console.log(artist);
-        //         return artist;
-        // }).catch( err => {
-        //     console.log(err);
-        // }))
-
+        //console.log(artist);
     
 }
+//getArtist('stevie wonder');
 
